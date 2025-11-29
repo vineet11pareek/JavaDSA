dcc32866bedc39f0d92321337591c71f8475f3bf
vineet11pareek <vineet00pareek@gmail.com>
2025-11-30 00:09:23 +0100

    Added optimisation on Rotate Array code

--- src/main/java/com/pattern/RotateArray.java
+++ src/main/java/com/pattern/RotateArray.java
@@ -1,16 +1,30 @@
 package com.pattern;
 
+import java.util.Arrays;
+
 public class RotateArray {
     public static void main(String[] args) {
         int [] arr = {2,5,7,21,34,37,41,42};
-        int [] newArr = rotateArray(arr,2);
-        for (int i : newArr) {
-            System.out.print(i+" ");
+        //From first Approach without optimisation
+        int [] arr1 = rotateArray(arr,2);
+        int [] arr2 = rotateArrayOptimise(arr,2);
 
-        }
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
     }
 
-    public static int[] rotateArray(int [] arr, int d){
+
+    /*
+    Time complexity  = O(n²), which is not good but solve the issue
+    below code runs:
+        * Outer loop: d times
+        * Inner loop: n times
+    Total = O(d × n)
+    If d = n, it becomes O(n²) → very slow for large arrays.
+    Space Complexity
+        * Uses only a few variables → O(1).
+     */
+    private static int[] rotateArray(int [] arr, int d){
         for (int i=0; i<d;i++){
             int temp = arr[0];
             for(int j =0;j<=arr.length-1;j++){
@@ -23,5 +37,35 @@ public class RotateArray {
             }
         }
         return arr;
+    }
+    /*
+    Rotate left by d using:
+1       ️* Reverse first d elements
+        * Reverse remaining n - d
+        * Reverse whole array
+    ->Time Complexity: O(n)
+    ->Space Complexity: O(1)
+    ->No extra array needed
+    ->Works even for large n
+     */
+
+    private static int[] rotateArrayOptimise(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;   // handle cases where d > n
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+
+        return arr;
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
