d319fe928f184dec411bd85c163e91aa2235429d
vineet11pareek <vineet00pareek@gmail.com>
2025-11-30 12:24:14 +0100

    LeetCode 1768:Merge Strings Alternately

---
+++ src/main/java/leetcode/MergeStringsAlternate.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/*
+1768: Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by
+adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional
+letters onto the end of the merged string.
+Return the merged string.
+
+    Example 1:
+        Input: word1 = "abc", word2 = "pqr"
+        Output: "apbqcr"
+        Explanation: The merged string will be merged as so:
+        word1:  a   b   c
+        word2:    p   q   r
+        merged: a p b q c r
+    Example 2:
+        Input: word1 = "ab", word2 = "pqrs"
+        Output: "apbqrs"
+        Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+        word1:  a   b
+        word2:    p   q   r   s
+        merged: a p b q   r   s
+
+ */
+
+public class MergeStringsAlternate {
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("ab", "pqrs"));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0, j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
+        }
+
+        if (i < word1.length()) {
+            sb.append(word1.substring(i));
+        }
+
+        if (j < word2.length()) {
+            sb.append(word2.substring(j));
+        }
+
+        return sb.toString();
+    }
+}
